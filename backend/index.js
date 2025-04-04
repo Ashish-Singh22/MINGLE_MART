3b3b8c141f96bcbd674a3bd737679111d0fa63a9
Ashish kumar Singh <139993841+Ashish-Singh22@users.noreply.github.com>
2025-04-04 14:13:27 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser')
 
 const app = express()
 app.use(cors({
-    origin : "https://mingle-mart-1v8t.vercel.app/",
+    origin : "https://mingle-mart-1v8t.vercel.app",
     credentials : true
 }))
 app.use(express.json({limit : '10mb'}));
